3f2f9a96f948e7b4b92df86234235f110527f1b7
Maarten <maarten.vandoorn@nmi-agro.nl>
2021-06-22 11:57:59 +0200

    change in date

--- R/loadknmi.R
+++ R/loadknmi.R
@@ -1,7 +1,7 @@
 #' Retrieve daily KNMI data for a specific weather station
 #'
-#' @param from_date Earliest date of the date range
-#' @param to_date Latest date of the date range
+#' @param from_date Earliest date of the date range (yyyy.mm.dd)
+#' @param to_date Latest date of the date range (yyyy.mm.dd)
 #' @param stationID ID of the weather station
 #' @param params Parameters of interest
 #' @examples
@@ -14,7 +14,12 @@ loadknmi <- function(from_date, to_date, stationID, params){
 
   #set date to a numeric value
   from_date <- as.numeric(gsub("\\.", "", from_date))
+  from_date <- as.numeric(gsub("_", "", from_date))
+  from_date <- as.numeric(gsub("-", "", from_date))
+
   to_date <- as.numeric(gsub("\\.", "", to_date))
+  to_date <- as.numeric(gsub("_", "", to_date))
+  to_date <- as.numeric(gsub("-", "", to_date))
 
   #create URL based on input
   URL <- paste0(baseURL, "?start=", from_date, "&end=", to_date, "&stns=", stationID)
@@ -29,7 +34,7 @@ loadknmi <- function(from_date, to_date, stationID, params){
   colnames(colss) <- "kolom"
     #create list to bind relevant lines to
   collist <- list()
-  for(i in 1:nrow(colss)){
+  for(i in 1:60){
     #loop through rows
     tmp <- colss[i,]
     #grepl patterns
@@ -51,7 +56,7 @@ loadknmi <- function(from_date, to_date, stationID, params){
   collist <- c("STN", "YYYYMMDD", collist)
 
   #set col names
-  if(ncol(knmi_table) == 41){
+  if(ncol(knmi_table) == length(collist)){
     colnames(knmi_table) <- collist
   } else{
     stop("Kolom aantal is niet gelijk aan 41")
