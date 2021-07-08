0f8176c559c1cf71cc3d5ff7817b83741cf9a7fe
maarvd <maarten.vandoorn@nmi-agro.nl>
2021-07-08 16:17:15 +0200

    add manual zoom to loadbasemap

--- R/loadbasemap.R
+++ R/loadbasemap.R
@@ -5,12 +5,13 @@
 #' @param source either "google", "esri" or "osm" as a source of the basemap. Defaults to "google".
 #' @param expand Extension factor of the bounding box If 1, the bounding box is unchanged. Values smaller than 1 reduces the bounding box,
 #' and values larger than 1 enlarges the bounding box. Defaults to 1.2.
+#' @param zoom Zoom level (defaults to 18)
 #' @examples
-#' basemap <- loadbasemap(parcels, "google", 1.2)
+#' basemap <- loadbasemap(parcels, "google", 1.2, 18)
 #'
 
 #' @export
-loadbasemap <- function(sf, source, expand){
+loadbasemap <- function(sf, source, expand, zoom){
   #set expand to 1.2 when missing
   if(missing(expand) == TRUE){
     expand <- 1.2
@@ -21,13 +22,18 @@ loadbasemap <- function(sf, source, expand){
     source <- "google"
   }
 
+  #set zoom to 18 when missing
+  if(missing(zoom) == TRUE){
+    zoom <- 18
+  }
+
   #load basemap based on the specified source
   if(source == "google"){
-      basemap <- tmaptools::read_osm(sf, type = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', zoom = 18, ext = expand)
+      basemap <- tmaptools::read_osm(sf, type = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', zoom = zoom, ext = expand)
     } else if(source == "esri"){
       basemap <- stars::read_stars("dev/esri_luchtfoto.tif")
     } else if(source == "osm"){
-      basemap <- tmaptools::read_osm(sf, type = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', zoom = 18, ext = expand)
+      basemap <- tmaptools::read_osm(sf, type = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png', zoom = zoom, ext = expand)
     }
 
   #return
